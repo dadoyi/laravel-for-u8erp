adf4abdfc58335df79c3169e2d5e36d2d85632d9
pu_li@loongjoy.com <pu_li@loongjoy.com>
2018-11-30 11:21:03 +0800

    基础发送完成

---
+++ src/Api.php
@@ -0,0 +1,149 @@
+<?php
+namespace ErpConnect;
+
+use Hanson\Foundation\AbstractAPI;
+use Illuminate\Config\Repository;
+use Illuminate\Support\Facades\Cache;
+
+class Api extends AbstractAPI
+{
+    /**
+     * base config
+     * @var Repository
+     */
+    protected $config;
+
+    /**
+     * erp token
+     * @var
+     */
+    protected $token;
+
+    /**
+     * send data
+     * @var
+     */
+    protected $data;
+
+    /**
+     * @var
+     */
+    protected $tokenkey;
+
+    /**
+     * methond url
+     */
+    const URL = 'https://api.yonyouup.com';
+
+    public function __construct(Repository $repository)
+    {
+        $this->config = $repository;
+        $this->checkHttp();
+        if(!Cache::get($this->gettokenkey())){
+            $this->getToken();
+        }
+    }
+
+    /**
+     * @throws \Hanson\Foundation\Exception\HttpException
+     */
+    public function getToken()
+    {
+        $http = $this->getHttp();
+        $this->tokenData();
+        $response = $http->get(self::URL.'/system/token',$this->data);
+
+        $result = json_decode(strval($response->getBody()), true);
+        $this->setToken($result['token']['id']);
+    }
+
+    /**
+     * @param $token
+     * @param int $time
+     */
+    protected function setToken($token,$time = 120)
+    {
+        if($time){
+            Cache::set($this->getTokenkey(),$token,$time);
+        }
+        $this->token = $token;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTokenkey()
+    {
+        if(is_null($this->tokenkey)){
+            return $this->data['from_account'];
+        }
+        return $this->tokenkey;
+    }
+
+
+    /**
+     * @param string $method
+     * @param array $params
+     * @return mixed
+     */
+    protected function get(string $method , array $params)
+    {
+        $params = array_merge($params,[
+            'token'=>$this->token
+        ]);
+
+        $response = $this->http->get(self::URL.$method, $params);
+
+        return $this->response($response);
+
+    }
+
+    /**
+     * @param string $method
+     * @param array $params
+     * @return mixed
+     */
+    protected function post(string $method , array $params)
+    {
+        $params = array_merge($params,[
+            'token'=>$this->token
+        ]);
+
+        $response = $this->http->post(self::URL.$method, $params);
+
+        return $this->response($response);
+    }
+
+
+    /**
+     * @return array
+     */
+    private function tokenData()
+    {
+        return $this->data = [
+            'from_account' => $this->config->get('erpconnect.FROM_ACCOUNT'),
+            'app_key' => $this->config->get('erpconnect.ERP_AppKey'),
+            'app_secret' => $this->config->get('erpconnect.ERP_AppSecret')
+        ];
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function response()
+    {
+        return json_decode(strval($response->getBody()), true);
+    }
+
+
+
+    /**
+     * @return \Hanson\Foundation\Http
+     */
+    public function checkHttp()
+    {
+        return $this->http ?: $this->getHttp();
+    }
+
+}
--- src/ErpConnect.php
+++ src/ErpConnect.php
@@ -2,64 +2,28 @@
 
 namespace ErpConnect;
 
-use Illuminate\Config\Repository;
+
 use Illuminate\Support\Facades\Cache;
 
-class ErpConnect extends Erp
+class ErpConnect extends Api
 {
-    /**
-     * 对应url code
-     * @var
-     */
-    protected $code;
 
-
-    /**
-     * 获取用户登录token
-     * @throws \Psr\SimpleCache\InvalidArgumentException
-     */
-    protected function getToken()
-    {
-        $this->request_url = sprintf($this->token_code,$this->from_account,$this->erp_key,$this->erp_secret);
-        Cache::set('erp_token',$this->get()->token->id,120);
-    }
-
-    /**
-     * 获取请求路径
-     */
-    protected function getUrl()
-    {
-        $str = $this->config->get('erpconnect.'.$this->code);
-//        $this->request_url =
-    }
-
-
-    public function is_token()
+    public function getTranSactionNum()
     {
 
     }
 
-
-    /**
-     * 设置
-     * @param $code
-     * @param $data
-     * @return $this
-     * @throws \Psr\SimpleCache\InvalidArgumentException
-     */
-    public function setData($code,$data)
+    public function getOrderStatus()
     {
-        if(!Cache::has('erp_token')){
-            $this->getToken();
-        }
-        $this->data = $data;
-        $this->code = $code;
-        $this->getUrl();
-        return $this;
+
     }
 
+    public function getDataOne()
+    {
 
-    public function splicingUrl()
+    }
+
+    public function getDataBetween()
     {
 
     }
